9c61b37145ce6cac13558eea0f6eb277d082052b
Nash Rojas <nash_rojas@ucne.edu.do>
2026-01-19 20:54:46 -0400

    Ejercicio 2.2

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -65,6 +67,19 @@ public class Main {
         System.out.println("boolean: " + ejemploBoolean);
         
         // Ejercicio 2.2
-        
+        System.out.println("");
+        System.out.println("Ejercicio 2.2");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingresa el primer número: ");
+        int num1 = sc.nextInt();
+        System.out.print("Ingresa el segundo número: ");
+        int num2 = sc.nextInt();
+
+        System.out.println("Suma: " + (num1 + num2));
+        System.out.println("Resta: " + (num1 - num2));
+        System.out.println("Multiplicación: " + (num1 * num2));
+        System.out.println("División: " + (num1 / num2));
+        System.out.println("Módulo: " + (num1 % num2));
+
     }
 }
